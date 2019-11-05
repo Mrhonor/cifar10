b24554e3792279e0874ea35f99b65318e62c206e
Mr <619500598@qq.com>
2019-11-05 13:48:52 +0800

    first train

--- DenseNetFunc.py
+++ DenseNetFunc.py
@@ -1,19 +1,5 @@
 import tensorflow as tf
-
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-import os
-
-backend = get_keras_submodule('backend')
-engine = get_keras_submodule('engine')
-layers = get_keras_submodule('layers')
-models = get_keras_submodule('models')
-keras_utils = get_keras_submodule('utils')
-
-
+import numpy as np
 
 def unpickle(file):
     import pickle
@@ -21,294 +7,82 @@ def unpickle(file):
         dict = pickle.load(fo, encoding='bytes')
     return dict
 
+# print(data_batch1.shape)
+# print(labels_batch1[0])
+# plt.imshow(data_batch1[0].transpose(1,2,0))
+# plt.axis("off")
+# plt.show()
 
-def dense_block(x, blocks, name):
-    """A dense block.
+def batch_norm(input):
+    axis = list(range(len(input.get_shape())-1))
+    mean, var = tf.nn.moments(input, axis)
+    bn = tf.nn.batch_normalization(input, mean, var, 0, 1, 1e-3)
+    return bn
 
-    # Arguments
-        x: input tensor.
-        blocks: integer, the number of building blocks.
-        name: string, block label.
-
-    # Returns
-        output tensor for the block.
-    """
-    for i in range(blocks):
-        x = conv_block(x, 32, name=name + '_block' + str(i + 1))
-    return x
-
-
-def transition_block(x, reduction, name):
-    """A transition block.
-
-    # Arguments
-        x: input tensor.
-        reduction: float, compression rate at transition layers.
-        name: string, block label.
-
-    # Returns
-        output tensor for the block.
-    """
-    bn_axis = 3 if backend.image_data_format() == 'channels_last' else 1
-    x = layers.BatchNormalization(axis=bn_axis, epsilon=1.001e-5,
-                                  name=name + '_bn')(x)
-    x = layers.Activation('relu', name=name + '_relu')(x)
-    x = layers.Conv2D(int(backend.int_shape(x)[bn_axis] * reduction), 1,
-                      use_bias=False,
-                      name=name + '_conv')(x)
-    x = layers.AveragePooling2D(2, strides=2, name=name + '_pool')(x)
-    return x
+    
+def onehot(y, num_classes=None):
+    y = np.array(y, dtype='int')
+    input_shape = y.shape
+    if input_shape and input_shape[-1] == 1 and len(input_shape) > 1:
+        input_shape = tuple(input_shape[:-1])
+    y = y.ravel()
+    if not num_classes:
+        num_classes = np.max(y) + 1
+    n = y.shape[0]
+    categorical = np.zeros((n, num_classes))
+    categorical[np.arange(n), y] = 1
+    output_shape = input_shape + (num_classes,)
+    categorical = np.reshape(categorical, output_shape)
+    return categorical
 
 
-def conv_block(x, growth_rate, name):
-    """A building block for a dense block.
+class cifar10:
+    def __init__(self):
+        batch1 = unpickle("../cifar-10-batches-py/data_batch_1")
+        self.data_batch1 = batch1[b'data'] 
+        self.data_batch1 = self.data_batch1.reshape((10000,3,32,32))
+        self.labels_batch1 = batch1.get(b'labels')
 
-    # Arguments
-        x: input tensor.
-        growth_rate: float, growth rate at dense layers.
-        name: string, block label.
+        batch2 = unpickle("../cifar-10-batches-py/data_batch_2")
+        self.data_batch2 = batch2[b'data'] 
+        self.data_batch2 = self.data_batch2.reshape((10000,3,32,32))
+        self.labels_batch2 = batch2.get(b'labels')
 
-    # Returns
-        Output tensor for the block.
-    """
-    bn_axis = 3 if backend.image_data_format() == 'channels_last' else 1
-    x1 = layers.BatchNormalization(axis=bn_axis,
-                                   epsilon=1.001e-5,
-                                   name=name + '_0_bn')(x)
-    x1 = layers.Activation('relu', name=name + '_0_relu')(x1)
-    x1 = layers.Conv2D(4 * growth_rate, 1,
-                       use_bias=False,
-                       name=name + '_1_conv')(x1)
-    x1 = layers.BatchNormalization(axis=bn_axis, epsilon=1.001e-5,
-                                   name=name + '_1_bn')(x1)
-    x1 = layers.Activation('relu', name=name + '_1_relu')(x1)
-    x1 = layers.Conv2D(growth_rate, 3,
-                       padding='same',
-                       use_bias=False,
-                       name=name + '_2_conv')(x1)
-    x = layers.Concatenate(axis=bn_axis, name=name + '_concat')([x, x1])
-    return x
+        batch3 = unpickle("../cifar-10-batches-py/data_batch_3")
+        self.data_batch3 = batch3[b'data'] 
+        self.data_batch3 = self.data_batch3.reshape((10000,3,32,32))
+        self.labels_batch3 = batch3.get(b'labels')
+
+        batch4 = unpickle("../cifar-10-batches-py/data_batch_4")
+        self.data_batch4 = batch4[b'data'] 
+        self.data_batch4 = self.data_batch4.reshape((10000,3,32,32))
+        self.labels_batch4 = batch4.get(b'labels')
+
+        batch5 = unpickle("../cifar-10-batches-py/data_batch_5")
+        self.data_batch5 = batch5[b'data'] 
+        self.data_batch5 = self.data_batch5.reshape((10000,3,32,32))
+        self.labels_batch5 = batch5.get(b'labels')
 
 
-def DenseNet(blocks,
-             include_top=True,
-             weights='imagenet',
-             input_tensor=None,
-             input_shape=None,
-             pooling=None,
-             classes=1000):
-    """Instantiates the DenseNet architecture.
-
-    Optionally loads weights pre-trained on ImageNet.
-    Note that the data format convention used by the model is
-    the one specified in your Keras config at `~/.keras/keras.json`.
-
-    # Arguments
-        blocks: numbers of building blocks for the four dense layers.
-        include_top: whether to include the fully-connected
-            layer at the top of the network.
-        weights: one of `None` (random initialization),
-              'imagenet' (pre-training on ImageNet),
-              or the path to the weights file to be loaded.
-        input_tensor: optional Keras tensor
-            (i.e. output of `layers.Input()`)
-            to use as image input for the model.
-        input_shape: optional shape tuple, only to be specified
-            if `include_top` is False (otherwise the input shape
-            has to be `(224, 224, 3)` (with `channels_last` data format)
-            or `(3, 224, 224)` (with `channels_first` data format).
-            It should have exactly 3 inputs channels.
-        pooling: optional pooling mode for feature extraction
-            when `include_top` is `False`.
-            - `None` means that the output of the model will be
-                the 4D tensor output of the
-                last convolutional layer.
-            - `avg` means that global average pooling
-                will be applied to the output of the
-                last convolutional layer, and thus
-                the output of the model will be a 2D tensor.
-            - `max` means that global max pooling will
-                be applied.
-        classes: optional number of classes to classify images
-            into, only to be specified if `include_top` is True, and
-            if no `weights` argument is specified.
-
-    # Returns
-        A Keras model instance.
-
-    # Raises
-        ValueError: in case of invalid argument for `weights`,
-            or invalid input shape.
-    """
-    if not (weights in {'imagenet', None} or os.path.exists(weights)):
-        raise ValueError('The `weights` argument should be either '
-                         '`None` (random initialization), `imagenet` '
-                         '(pre-training on ImageNet), '
-                         'or the path to the weights file to be loaded.')
-
-    if weights == 'imagenet' and include_top and classes != 1000:
-        raise ValueError('If using `weights` as imagenet with `include_top`'
-                         ' as true, `classes` should be 1000')
-
-    # Determine proper input shape
-    input_shape = _obtain_input_shape(input_shape,
-                                      default_size=224,
-                                      min_size=221,
-                                      data_format=backend.image_data_format(),
-                                      require_flatten=include_top,
-                                      weights=weights)
-
-    if input_tensor is None:
-        img_input = layers.Input(shape=input_shape)
-    else:
-        if not backend.is_keras_tensor(input_tensor):
-            img_input = layers.Input(tensor=input_tensor, shape=input_shape)
-        else:
-            img_input = input_tensor
-
-    bn_axis = 3 if backend.image_data_format() == 'channels_last' else 1
-
-    x = layers.ZeroPadding2D(padding=((3, 3), (3, 3)))(img_input)
-    x = layers.Conv2D(64, 7, strides=2, use_bias=False, name='conv1/conv')(x)
-    x = layers.BatchNormalization(
-        axis=bn_axis, epsilon=1.001e-5, name='conv1/bn')(x)
-    x = layers.Activation('relu', name='conv1/relu')(x)
-    x = layers.ZeroPadding2D(padding=((1, 1), (1, 1)))(x)
-    x = layers.MaxPooling2D(3, strides=2, name='pool1')(x)
-
-    x = dense_block(x, blocks[0], name='conv2')
-    x = transition_block(x, 0.5, name='pool2')
-    x = dense_block(x, blocks[1], name='conv3')
-    x = transition_block(x, 0.5, name='pool3')
-    x = dense_block(x, blocks[2], name='conv4')
-    x = transition_block(x, 0.5, name='pool4')
-    x = dense_block(x, blocks[3], name='conv5')
-
-    x = layers.BatchNormalization(
-        axis=bn_axis, epsilon=1.001e-5, name='bn')(x)
-
-    if include_top:
-        x = layers.GlobalAveragePooling2D(name='avg_pool')(x)
-        x = layers.Dense(classes, activation='softmax', name='fc1000')(x)
-    else:
-        if pooling == 'avg':
-            x = layers.GlobalAveragePooling2D(name='avg_pool')(x)
-        elif pooling == 'max':
-            x = layers.GlobalMaxPooling2D(name='max_pool')(x)
-
-    # Ensure that the model takes into account
-    # any potential predecessors of `input_tensor`.
-    if input_tensor is not None:
-        inputs = engine.get_source_inputs(input_tensor)
-    else:
-        inputs = img_input
-
-    # Create model.
-    if blocks == [6, 12, 24, 16]:
-        model = models.Model(inputs, x, name='densenet121')
-    elif blocks == [6, 12, 32, 32]:
-        model = models.Model(inputs, x, name='densenet169')
-    elif blocks == [6, 12, 48, 32]:
-        model = models.Model(inputs, x, name='densenet201')
-    else:
-        model = models.Model(inputs, x, name='densenet')
-
-    # Load weights.
-    if weights == 'imagenet':
-        if include_top:
-            if blocks == [6, 12, 24, 16]:
-                weights_path = keras_utils.get_file(
-                    'densenet121_weights_tf_dim_ordering_tf_kernels.h5',
-                    DENSENET121_WEIGHT_PATH,
-                    cache_subdir='models',
-                    file_hash='0962ca643bae20f9b6771cb844dca3b0')
-            elif blocks == [6, 12, 32, 32]:
-                weights_path = keras_utils.get_file(
-                    'densenet169_weights_tf_dim_ordering_tf_kernels.h5',
-                    DENSENET169_WEIGHT_PATH,
-                    cache_subdir='models',
-                    file_hash='bcf9965cf5064a5f9eb6d7dc69386f43')
-            elif blocks == [6, 12, 48, 32]:
-                weights_path = keras_utils.get_file(
-                    'densenet201_weights_tf_dim_ordering_tf_kernels.h5',
-                    DENSENET201_WEIGHT_PATH,
-                    cache_subdir='models',
-                    file_hash='7bb75edd58cb43163be7e0005fbe95ef')
-        else:
-            if blocks == [6, 12, 24, 16]:
-                weights_path = keras_utils.get_file(
-                    'densenet121_weights_tf_dim_ordering_tf_kernels_notop.h5',
-                    DENSENET121_WEIGHT_PATH_NO_TOP,
-                    cache_subdir='models',
-                    file_hash='4912a53fbd2a69346e7f2c0b5ec8c6d3')
-            elif blocks == [6, 12, 32, 32]:
-                weights_path = keras_utils.get_file(
-                    'densenet169_weights_tf_dim_ordering_tf_kernels_notop.h5',
-                    DENSENET169_WEIGHT_PATH_NO_TOP,
-                    cache_subdir='models',
-                    file_hash='50662582284e4cf834ce40ab4dfa58c6')
-            elif blocks == [6, 12, 48, 32]:
-                weights_path = keras_utils.get_file(
-                    'densenet201_weights_tf_dim_ordering_tf_kernels_notop.h5',
-                    DENSENET201_WEIGHT_PATH_NO_TOP,
-                    cache_subdir='models',
-                    file_hash='1c2de60ee40562448dbac34a0737e798')
-        model.load_weights(weights_path)
-    elif weights is not None:
-        model.load_weights(weights)
-
-    return model
-
-
-def DenseNet121(include_top=True,
-                weights='imagenet',
-                input_tensor=None,
-                input_shape=None,
-                pooling=None,
-                classes=1000):
-    return DenseNet([6, 12, 24, 16],
-                    include_top, weights,
-                    input_tensor, input_shape,
-                    pooling, classes)
-
-
-def DenseNet169(include_top=True,
-                weights='imagenet',
-                input_tensor=None,
-                input_shape=None,
-                pooling=None,
-                classes=1000):
-    return DenseNet([6, 12, 32, 32],
-                    include_top, weights,
-                    input_tensor, input_shape,
-                    pooling, classes)
-
-
-def DenseNet201(include_top=True,
-                weights='imagenet',
-                input_tensor=None,
-                input_shape=None,
-                pooling=None,
-                classes=1000):
-    return DenseNet([6, 12, 48, 32],
-                    include_top, weights,
-                    input_tensor, input_shape,
-                    pooling, classes)
-
-
-def preprocess_input(x, data_format=None):
-    """Preprocesses a numpy array encoding a batch of images.
-
-    # Arguments
-        x: a 3D or 4D numpy array consists of RGB values within [0, 255].
-        data_format: data format of the image tensor.
-
-    # Returns
-        Preprocessed array.
-    """
-    return imagenet_utils.preprocess_input(x, data_format, mode='torch')
-
-
-setattr(DenseNet121, '__doc__', DenseNet.__doc__)
-setattr(DenseNet169, '__doc__', DenseNet.__doc__)
-setattr(DenseNet201, '__doc__', DenseNet.__doc__)
+    def get_next_batch(self, BATCH_SIZE, i):
+        num = i*BATCH_SIZE % 40000
+        if num <= 10000-BATCH_SIZE:
+            x = self.data_batch1[num:num+BATCH_SIZE]
+            y = onehot(self.labels_batch1[num:num+BATCH_SIZE], 10)
+            return x, y
+        if num > 10000-BATCH_SIZE and num <= 20000-BATCH_SIZE:
+            num = num % 10000
+            x = self.data_batch2[num:num+BATCH_SIZE]
+            y = onehot(self.labels_batch2[num:num+BATCH_SIZE], 10)
+            return x, y
+        if num > 20000-BATCH_SIZE and num <= 30000-BATCH_SIZE:
+            num = num % 10000
+            x = self.data_batch3[num:num+BATCH_SIZE]
+            y = onehot(self.labels_batch3[num:num+BATCH_SIZE], 10)
+            return x, y
+        if num > 30000-BATCH_SIZE and num <= 40000-BATCH_SIZE:
+            num = num % 10000
+            x = self.data_batch4[num:num+BATCH_SIZE]
+            y = onehot(self.labels_batch4[num:num+BATCH_SIZE], 10)
+            return x, y    
+    
