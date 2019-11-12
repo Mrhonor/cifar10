7b5456d2587b8997e08bbc9ebb7298b47abc0e80
Mr <619500598@qq.com>
2019-11-12 16:52:29 +0800

    加入test数据导入测试

--- DenseNetFunc.py
+++ DenseNetFunc.py
@@ -1,51 +1,118 @@
 import tensorflow as tf
-import DenseNet_inference
-import DenseNet_train
-import DenseNetFunc
+import numpy as np
 
-BATCH_SIZE = 50
-MODEL_SAVE_PATH = DenseNet_train.MODEL_SAVE_PATH
-GRAPH_SAVE_PATH ="graph/"
+def unpickle(file):
+    import pickle
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
 
-def evaluate(dataset):
-    with tf.Graph().as_default() as g:
-        x  = tf.placeholder(tf.float32, 
-            [BATCH_SIZE, 
-            DenseNet_train.IMAGE_SIZE,
-            DenseNet_train.IMAGE_SIZE,
-            DenseNet_inference.NUM_CHANNELS],
+DECAY = 0.9
 
-            name="x-input")
-        y_labels = tf.placeholder(tf.float32, [None, DenseNet_inference.NUM_LABELS], name='y-input')
+def batch_norm(input, train, NUM, type):
+    if type==1:
+        pop_mean = tf.get_variable("pop_mean1", [NUM], trainable=False, initializer=tf.constant_initializer(0.0))
+        pop_var = tf.get_variable("pop_var1", [NUM], trainable=False, initializer=tf.constant_initializer(1.0))
+    else:
+        pop_mean = tf.get_variable("pop_mean2", [NUM], trainable=False, initializer=tf.constant_initializer(0.0))
+        pop_var = tf.get_variable("pop_var2", [NUM], trainable=False, initializer=tf.constant_initializer(1.0))
+
+    if train==True:
+        axis = list(range(len(input.get_shape())-1))
+        mean, var = tf.nn.moments(input, axis)
+
+        mean = tf.assign(pop_mean, pop_mean*DECAY + mean*(1 - DECAY))
+        var = tf.assign(pop_var, pop_var*DECAY + var*(1 - DECAY))
+        with tf.control_dependencies([mean, var]):
+            bn = tf.nn.batch_normalization(input, mean, var, 0, 1, 1e-3)
+    else:
+        bn = tf.nn.batch_normalization(input, pop_mean, pop_var, 0, 1, 1e-3)
+    return bn
+
+    
+def onehot(y, num_classes=None):
+    y = np.array(y, dtype='int')
+    input_shape = y.shape
+    if input_shape and input_shape[-1] == 1 and len(input_shape) > 1:
+        input_shape = tuple(input_shape[:-1])
+    y = y.ravel()
+    if not num_classes:
+        num_classes = np.max(y) + 1
+    n = y.shape[0]
+    categorical = np.zeros((n, num_classes))
+    categorical[np.arange(n), y] = 1
+    output_shape = input_shape + (num_classes,)
+    categorical = np.reshape(categorical, output_shape)
+    return categorical
 
 
-        y = DenseNet_inference.inference(x, True, None)
 
-        correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_labels, 1))
-        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+class cifar10:
+    def __init__(self):
+        batch1 = unpickle("../cifar-10-batches-py/data_batch_1")
+        self.data_batch1 = batch1[b'data'] 
+        self.data_batch1 = self.data_batch1.reshape((10000,3,32,32)) 
+        self.labels_batch1 = batch1.get(b'labels')
 
-        saver = tf.train.Saver()
+        batch2 = unpickle("../cifar-10-batches-py/data_batch_2")
+        self.data_batch2 = batch2[b'data'] 
+        self.data_batch2 = self.data_batch2.reshape((10000,3,32,32)) 
+        self.labels_batch2 = batch2.get(b'labels')
 
-        with tf.Session() as sess:
-            saver.restore(sess, MODEL_SAVE_PATH + "model29900.ckpt")
-            summary_write = tf.summary.FileWriter(GRAPH_SAVE_PATH, tf.get_default_graph())
-            i = 0
-            acc = 0
-            while i*BATCH_SIZE < 10000 - BATCH_SIZE:
-                xs, ys = list(dataset.get_val_batch(i*BATCH_SIZE, i*BATCH_SIZE + BATCH_SIZE))
-                # xs, ys = list(dataset.get_next_batch(BATCH_SIZE, i))
-                xs = xs.transpose(0,2,3,1)
-                accuracy_score = sess.run(accuracy, feed_dict={x:xs, y_labels:ys})
-                i = i + 1
-                acc = acc + accuracy_score
-            acc = acc / i
-            print("acc: %f"%(acc))
+        batch3 = unpickle("../cifar-10-batches-py/data_batch_3")
+        self.data_batch3 = batch3[b'data'] 
+        self.data_batch3 = self.data_batch3.reshape((10000,3,32,32)) 
+        self.labels_batch3 = batch3.get(b'labels')
 
+        batch4 = unpickle("../cifar-10-batches-py/data_batch_4")
+        self.data_batch4 = batch4[b'data'] 
+        self.data_batch4 = self.data_batch4.reshape((10000,3,32,32)) 
+        self.labels_batch4 = batch4.get(b'labels')
 
-def main(argv=None):
-    cifar = DenseNetFunc.cifar10()
-    evaluate(cifar)
+        batch5 = unpickle("../cifar-10-batches-py/data_batch_5")
+        self.data_batch5 = batch5[b'data'] 
+        self.data_batch5 = self.data_batch5.reshape((10000,3,32,32)) 
+        self.labels_batch5 = batch5.get(b'labels')
 
+        test_batch = unpickle("../cifar-10-batches-py/data_batch_5")
+        self.test_data = test_batch[b'data']
+        self.test_data = self.test_data.reshape((10000,3,32,32)) 
+        self.labels_test = test_batch.get(b'labels')
 
-if __name__ == '__main__':
-    tf.app.run()
+    def get_next_batch(self, BATCH_SIZE, i):
+        num = i*BATCH_SIZE % 40000
+        if num <= 10000-BATCH_SIZE:
+            x = self.data_batch1[num:num+BATCH_SIZE]
+            y = onehot(self.labels_batch1[num:num+BATCH_SIZE], 10)
+            return x, y
+        if num > 10000-BATCH_SIZE and num <= 20000-BATCH_SIZE:
+            num = num % 10000
+            x = self.data_batch2[num:num+BATCH_SIZE]
+            y = onehot(self.labels_batch2[num:num+BATCH_SIZE], 10)
+            return x, y
+        if num > 20000-BATCH_SIZE and num <= 30000-BATCH_SIZE:
+            num = num % 10000
+            x = self.data_batch3[num:num+BATCH_SIZE]
+            y = onehot(self.labels_batch3[num:num+BATCH_SIZE], 10)
+            return x, y
+        if num > 30000-BATCH_SIZE and num <= 40000-BATCH_SIZE:
+            num = num % 10000
+            x = self.data_batch4[num:num+BATCH_SIZE]
+            y = onehot(self.labels_batch4[num:num+BATCH_SIZE], 10)
+            return x, y    
+    
+    def get_val_batch(self, num1, num2):
+        if num1 >= 10000 or num2 >= 10000 or num1 > num2:
+            return None
+        
+        x = self.data_batch5[num1:num2]
+        y = onehot(self.labels_batch5[num1:num2], 10)
+        return x, y
+
+    def get_test_batch(self, num1, num2):
+        if num1 >= 10000 or num2 >= 10000 or num1 > num2:
+            return None
+        
+        x = self.test_data[num1:num2]
+        y = onehot(self.labels_test[num1:num2], 10)
+        return x, y
--- DenseNet_test.py
+++ DenseNet_test.py
@@ -7,6 +7,7 @@ import msvcrt
 import numpy as np
 
 BATCH_SIZE = 500
+BEST_MODE_SAVE_PATH = "model/model13/model29900.ckpt"
 
 def evaluate(dataset):
     with tf.Graph().as_default() as g:
@@ -22,36 +23,26 @@ def evaluate(dataset):
 
         y = DenseNet_inference.inference(x, None, None)
 
-        correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_labels, 1))
-        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
         saver = tf.train.Saver()
 
         with tf.Session() as sess:
-            ckpt = tf.train.get_checkpoint_state(DenseNet_train.MODEL_SAVE_PATH)
-            if ckpt and ckpt.model_checkpoint_path:
-                saver.restore(sess, ckpt.model_checkpoint_path)
-                # xs, ys = list(dataset.get_val_batch(1000, 1000+BATCH_SIZE))
-                # xs = xs.transpose(0,2,3,1)
-                # accuracy_score = sess.run(accuracy, feed_dict={x:xs, y_labels:ys})
-                # print("accuracy = %g" %(accuracy_score))
-                # print(sess.run(y, feed_dict={x:xs}))
-                i = 0
-                xs, ys = list(dataset.get_val_batch(0, BATCH_SIZE))
-                xs = xs.transpose(0,2,3,1)
-                outcome = sess.run(y, feed_dict={x:xs})
-                while True:
-                    # print("输出结果为：label: %d"%(tf.argmax(outcome, 1).eval()))
-                    print(outcome[i])
-                    print("模型给出的label：%d"%(np.argmax(outcome[i])))
-                    print("真正的标签为 : %d"%(np.argmax(ys[i])))
-                    plt.imshow(xs[i])
-                    plt.axis("off")
-                    plt.show()
-                    if ord(msvcrt.getch()) in [68, 100]:
-                        break
-                    i = i + 1
- 
+            saver.restore(sess, BEST_MODE_SAVE_PATH)
+            i = 0
+            xs, ys = list(dataset.get_test_batch(0, BATCH_SIZE))
+            xs = xs.transpose(0,2,3,1)
+            outcome = sess.run(y, feed_dict={x:xs})
+            while True:
+                # print("输出结果为：label: %d"%(tf.argmax(outcome, 1).eval()))
+                print(outcome[i])
+                print("模型给出的label：%d"%(np.argmax(outcome[i])))
+                print("真正的标签为 : %d"%(np.argmax(ys[i])))
+                plt.imshow(xs[i])
+                plt.axis("off")
+                plt.show()
+                if ord(msvcrt.getch()) in [68, 100]:
+                    break
+                i = i + 1
 
             else:
                 print('No checkpoint file found')
